34b1dd88d339a064b35a95b762b0bc397146a695
sasladic <sasa.sladic@s7designcreative.com>
2019-09-24 11:02:55 +0200

    Append

--- app/User.php
+++ app/User.php
@@ -2,12 +2,13 @@
 
 namespace App;
 
+use Illuminate\Contracts\Auth\CanResetPassword;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Auth;
 
-class User extends Authenticatable implements MustVerifyEmail
+class User extends Authenticatable implements MustVerifyEmail, CanResetPassword
 {
     use Notifiable;
 
