d5bcb9033f3ecd2a42a224f53608548165108ba2
Sasa Sladic <sasladic@s7designcreative.com>
2020-04-04 00:28:17 +0200

    Deleted token

--- app/Notifications/PasswordResetNotification.php
+++ app/Notifications/PasswordResetNotification.php
@@ -51,7 +51,7 @@ class PasswordResetNotification extends Notification
             ->line(Lang::get('You are receiving this email because we received a password reset request for your account.'))
             ->action(Lang::get('Reset Password'), $urlToResetForm)
             ->line(Lang::get('This password reset link will expire in :count minutes.', ['count' => config('auth.passwords.'.config('auth.defaults.passwords').'.expire')]))
-            ->line(Lang::get('If you did not request a password reset, no further action is required.Token: ==> ' .$this->token));
+            ->line(Lang::get('If you did not request a password reset, no further action is required.'));
     }
 
     /**
